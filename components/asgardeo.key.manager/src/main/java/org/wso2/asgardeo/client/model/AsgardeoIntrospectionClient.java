d94053f1185ceb8fc9d1d760962a8efdf32a5165
Shakthi Weerawansa <157775686+ShakW101@users.noreply.github.com>
2026-01-21 12:54:20 +0530

    models for introspection created

---
+++ components/asgardeo.key.manager/src/main/java/org/wso2/asgardeo/client/model/AsgardeoIntrospectionClient.java
@@ -0,0 +1,15 @@
+package org.wso2.asgardeo.client.model;
+
+import feign.Headers;
+import feign.Param;
+import feign.RequestLine;
+
+public interface AsgardeoIntrospectionClient {
+    @RequestLine("POST")
+    @Headers({
+            "Content-Type: application/x-www-form-urlencoded",
+            "Authorization: Basic {basic}"
+    })
+    AsgardeoIntrospectionResponse introspect(@Param("token") String token,
+                                             @Param("basic") String basic);
+}
---
+++ components/asgardeo.key.manager/src/main/java/org/wso2/asgardeo/client/model/AsgardeoIntrospectionResponse.java
@@ -0,0 +1,139 @@
+package org.wso2.asgardeo.client.model;
+
+import com.google.gson.annotations.SerializedName;
+
+public class AsgardeoIntrospectionResponse {
+
+    @SerializedName("active")
+    private boolean active;
+
+    @SerializedName("scope")
+    private String scope;
+
+    @SerializedName("client_id")
+    private String clientId;
+
+    @SerializedName("token_type")
+    private String tokenType;
+
+    @SerializedName("aut")
+    private String aut; // APPLICATION
+
+    @SerializedName("exp")
+    private Long exp;
+
+    @SerializedName("iat")
+    private Long iat;
+
+    @SerializedName("nbf")
+    private Long nbf;
+
+    @SerializedName("iss")
+    private String iss;
+
+    @SerializedName("sub")
+    private String sub;
+
+    @SerializedName("aud")
+    private String aud;
+
+    @SerializedName("jti")
+    private String jti;
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public String getScope() {
+        return scope;
+    }
+
+    public void setScope(String scope) {
+        this.scope = scope;
+    }
+
+    public String getClientId() {
+        return clientId;
+    }
+
+    public void setClientId(String clientId) {
+        this.clientId = clientId;
+    }
+
+    public String getTokenType() {
+        return tokenType;
+    }
+
+    public void setTokenType(String tokenType) {
+        this.tokenType = tokenType;
+    }
+
+    public String getAut() {
+        return aut;
+    }
+
+    public void setAut(String aut) {
+        this.aut = aut;
+    }
+
+    public Long getExp() {
+        return exp;
+    }
+
+    public void setExp(Long exp) {
+        this.exp = exp;
+    }
+
+    public Long getIat() {
+        return iat;
+    }
+
+    public void setIat(Long iat) {
+        this.iat = iat;
+    }
+
+    public Long getNbf() {
+        return nbf;
+    }
+
+    public void setNbf(Long nbf) {
+        this.nbf = nbf;
+    }
+
+    public String getIss() {
+        return iss;
+    }
+
+    public void setIss(String iss) {
+        this.iss = iss;
+    }
+
+    public String getSub() {
+        return sub;
+    }
+
+    public void setSub(String sub) {
+        this.sub = sub;
+    }
+
+    public String getAud() {
+        return aud;
+    }
+
+    public void setAud(String aud) {
+        this.aud = aud;
+    }
+
+    public String getJti() {
+        return jti;
+    }
+
+    public void setJti(String jti) {
+        this.jti = jti;
+    }
+}
+
